9f1bd0ebb238ee1b8a5b003ad613ef3250fdd7c4
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-09-29 05:44:32 -0500

    Add mercury.jpg and roxale font

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Mercury from './mercury.jpg';
 
 function component() {
     const element = document.createElement('div');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    const img = new Image();
+    img.src = Mercury;
+    
+    element.appendChild(img);
   
     return element;
   }
